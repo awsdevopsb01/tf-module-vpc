455dc1d231973beec3d00e5b12bdc70a163df0cd
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2024-04-03 06:49:33 +0200

    subnets - tf-roboshop

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ module "subnets" {
   vpc_id = aws_vpc.main.id
   cidr_block = each.value["cidr_block"]
   name = each.value["name"]
+  azi=each.value["azi"]
 
   tags=var.tags
   env=var.env
--- subnets/main.tf
+++ subnets/main.tf
@@ -2,7 +2,8 @@ resource "aws_subnet" "main" {
   count = length(var.cidr_block)
   vpc_id     = var.vpc_id
   cidr_block = var.cidr_block[count.index]
+  availability_zone = var.azi[count.index]
 
-  tags = merge(var.tags, {Name="${var.env}-${var.name}-subnet-${count.index}"})
+  tags = merge(var.tags, {Name="${var.env}-${var.name}-subnet-${count.index}+1"})
 
 }
